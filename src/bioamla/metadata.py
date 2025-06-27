5c3a72c5b4f38df7a7960c2eeddd6f8f92df59c8
John McMeen <johnmcmeen@gmail.com>
2025-06-26 23:04:09 -0400

    v 0.0.7

--- src/bioamla/metadata.py
+++ src/bioamla/metadata.py
@@ -3,9 +3,9 @@ from novus_pytils.wave import get_wav_file_metadata
 
 @click.command()
 @click.argument('filepath')
-def metadata(filepath):
+def print_metadata(filepath):
     metadata = get_wav_file_metadata(filepath)
     click.echo(f"{metadata}")
 
 if __name__ == '__main__':
-    metadata()
+    print_metadata()
