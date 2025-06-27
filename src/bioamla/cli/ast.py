02f132834d2ffea476ceaac92872f401996463c3
John McMeen <johnmcmeen@gmail.com>
2025-06-27 01:10:31 -0400

    Update ast.py

--- src/bioamla/cli/ast.py
+++ src/bioamla/cli/ast.py
@@ -1,5 +1,9 @@
 import click
 from novus_pytils.files import directory_exists, create_directory, copy_directory
+from pathlib import Path
+
+module_dir = Path(__file__).parent
+config_dir = module_dir.joinpath("config")
 
 @click.command()
 @click.argument('filepath')
@@ -8,7 +12,8 @@ def main(filepath):
         raise ValueError("Existing directory")
 
     create_directory(filepath)
-    copy_directory("../config/", filepath)
+    # click.echo(module_dir)
+    copy_directory(config_dir, filepath)
 
     click.echo(f"AST project created at {filepath}")
 
