2fc8ddb8903a5b9f815bb30a8e3f8709cad1de8d
John McMeen <johnmcmeen@gmail.com>
2025-06-27 02:38:48 -0400

    Create gpu.py

---
+++ src/bioamla/cli/gpu.py
@@ -0,0 +1,17 @@
+import click
+import torch
+
+@click.command()
+def main():
+    cuda_available = torch.cuda.is_available()
+    current_device = torch.cuda.current_device()
+    device_count = torch.cuda.device_count()
+    device_name = torch.cuda.get_device_name(0)
+
+    click.echo(f'cuda available: {cuda_available}')
+    click.echo(f'current device: {current_device}')
+    click.echo(f'device count: {device_count}')
+    click.echo(f'device name: {device_name}')
+
+if __name__ == '__main__':
+    main()
