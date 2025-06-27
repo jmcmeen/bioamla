fbf3b561722eabcee42b9661ebec69ddf44906c0
John McMeen <johnmcmeen@gmail.com>
2025-06-26 22:52:19 -0400

    v 0.0.5

--- src/bioamla/metadata.py
+++ src/bioamla/metadata.py
@@ -1,10 +1,11 @@
 import click
+from novus_pytils.wave import get_wav_file_metadata
 
 @click.command()
-@click.option('--name', default='World', help='Name to greet.')
-def main(name):
-    """Simple CLI tool to greet a name."""
-    click.echo(f"Hello, {name}!")
+@click.argument('filepath')
+def metadata(filepath):
+    metadata = get_wav_file_metadata(filepath)
+    click.echo(f"{metadata}")
 
 if __name__ == '__main__':
-    main()
+    metadata()
