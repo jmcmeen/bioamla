94e647f4da6291be06e00242461a537c0bc4ff4d
John McMeen <johnmcmeen@gmail.com>
2025-06-27 03:17:47 -0400

    Update batch.py

--- src/bioamla/core/batch.py
+++ src/bioamla/core/batch.py
@@ -1,8 +1,9 @@
 # bioamla/batch.py
 import pandas as pd
+from novus_pytils.wave import get_wav_files_metadatam, get_wav_files
 
 def get_wav_file_frame(dir: str) -> pd.DataFrame:
-    from bioamla.wav import get_wav_files, get_wav_files_metadata
+
     wav_files_metadata = get_wav_files_metadata(get_wav_files(dir))
     df = pd.DataFrame(wav_files_metadata, columns=['filepath', 'file_size', 'num_channels', 'sample_width', 'frame_rate', 'num_frames', 'duration'])
     return df
