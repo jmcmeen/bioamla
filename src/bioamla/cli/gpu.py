ae74f42a459f37d72cfd7d342e45158bbf8a530b
John McMeen <johnmcmeen@gmail.com>
2025-06-27 02:44:17 -0400

    Update gpu.py

--- src/bioamla/cli/gpu.py
+++ src/bioamla/cli/gpu.py
@@ -7,14 +7,15 @@ def main():
     current_device = torch.cuda.current_device()
     device_count = torch.cuda.device_count()
     device_name = torch.cuda.get_device_name(0)
+    click.echo(f"GPU system info:")
 
-    click.echo(f'cuda available: {cuda_available}')
-    click.echo(f'current device: {current_device}')
-    click.echo(f'device count: {device_count}')
-    
+    click.echo(f'CUDA available: {cuda_available}')
+    click.echo(f'Current device: {current_device}')
+    click.echo(f'Device count: {device_count}')
+
     for i in range(device_count):
         device_name = torch.cuda.get_device_name(i)
-        click.echo(f'device name: {device_name}')
+        click.echo(f'Device {i}: {device_name}')
 
 if __name__ == '__main__':
     main()
