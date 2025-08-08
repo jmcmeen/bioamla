d095e73cc8b2e5f72c2db38b5f86715eefbc177a
John McMeen <johnmcmeen@gmail.com>
2025-08-08 18:34:07 -0400

    dev code

---
+++ src/bioamla/ui/__init__.py
@@ -0,0 +1,26 @@
+"""
+User interface components for the audio editor application.
+"""
+from .audio_widgets import (
+    WaveformDisplay,
+    SpectrogramDisplay,
+    AudioInfoPanel,
+    TransportControls,
+    FilterPanel,
+    AnnotationPanel,
+    FileOperationsPanel,
+    StatusBar,
+    ProgressDisplay
+)
+
+__all__ = [
+    "WaveformDisplay",
+    "SpectrogramDisplay", 
+    "AudioInfoPanel",
+    "TransportControls",
+    "FilterPanel",
+    "AnnotationPanel",
+    "FileOperationsPanel",
+    "StatusBar",
+    "ProgressDisplay"
+]
