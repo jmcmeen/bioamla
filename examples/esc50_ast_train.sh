7071a3108bb2787a3effc4fa39864252816f201b
John McMeen <johnmcmeen@gmail.com>
2025-12-14 15:34:19 -0500

    Update esc50_ast_train.sh

--- examples/esc50_ast_train.sh
+++ examples/esc50_ast_train.sh
@@ -1,5 +1,12 @@
 #example workflow for esc50
 bioamla version
 bioamla devices
-bioamla ast-finetune --training-dir "esc50" --train-dataset "ashraq/esc50" --num-train-epochs 1 --per-device-train-batch-size 8
+bioamla ast-finetune \
+  --training-dir "esc50" \
+  --train-dataset "ashraq/esc50" \
+  --num-train-epochs 1 \
+  --per-device-train-batch-size 8 \
+  --fp16 \
+  --gradient-accumulation-steps 2 \
+  --dataloader-num-workers 4
 
