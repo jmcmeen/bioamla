9cb1ff751dddb12cf02b047e24493aaca70ad6d0
John McMeen <johnmcmeen@gmail.com>
2025-12-14 15:34:15 -0500

    Update scp_frogs_ast_train.sh

--- examples/scp_frogs_ast_train.sh
+++ examples/scp_frogs_ast_train.sh
@@ -1,5 +1,13 @@
 #example workflow for SCP frogs
 bioamla version
 bioamla devices
-bioamla ast-finetune --training-dir "scp-frogs" --train-dataset "bioamla/scp-frogs" --num-train-epochs 50
+bioamla ast-finetune \
+  --training-dir "scp-frogs" \
+  --train-dataset "bioamla/scp-frogs" \
+  --num-train-epochs 50 \
+  --per-device-train-batch-size 8 \
+  --fp16 \
+  --gradient-accumulation-steps 2 \
+  --dataloader-num-workers 4 \
+  --torch-compile
 
