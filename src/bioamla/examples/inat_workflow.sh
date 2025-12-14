aa54d3ef74463e00c982eb6daea7b2ef30c2592b
John McMeen <johnmcmeen@gmail.com>
2025-12-14 03:19:22 -0500

    Update inat_workflow.sh

---
+++ src/bioamla/examples/inat_workflow.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+bioamla inat-audio ./dev_data/test2 \
+    --taxon-ids "24268, 65982, 23930, 24263, 65979, 66002, 66012, 60341, 64968, 64977, 24256" \
+    --quality-grade research \
+    --obs-per-taxon 2
+
