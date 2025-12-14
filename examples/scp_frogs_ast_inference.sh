4dd7fd817d639a2f6b5e490f1f0ed570cdcc5b4f
John McMeen <johnmcmeen@gmail.com>
2025-12-14 14:50:29 -0500

    updating scripts

--- examples/scp_frogs_ast_inference.sh
+++ examples/scp_frogs_ast_inference.sh
@@ -1,7 +1,4 @@
 #example workflow for SCP frogs
-bioamla version
-bioamla devices
-bioamla ast-finetune --training-dir "scp-frogs" --train-dataset "bioamla/scp-frogs" --num-train-epochs 50
 bioamla download "https://www.bioamla.org/datasets/scp_small.zip" "scp_small.zip" 
 bioamla unzip "scp_small.zip" . 
 bioamla ast-batch-inference "scp_small" --model-path "scp-frogs/best_model"
