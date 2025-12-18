33af1207b36af1835567f5a5a206858829bb763e
John McMeen <johnmcmeen@gmail.com>
2025-12-18 01:02:50 -0500

    moving tui to dev

---
+++ src/bioamla/dev/__init__.py
@@ -0,0 +1,5 @@
+"""Development and experimental features for bioamla.
+
+This package contains experimental features that are not yet ready for
+production use. APIs in this package may change without notice.
+"""
