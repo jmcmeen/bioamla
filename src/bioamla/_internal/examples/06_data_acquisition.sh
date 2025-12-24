66c6c1cd61bcbf56f6ddd981c76b018ea7d2e91e
John McMeen <johnmcmeen@gmail.com>
2025-12-24 03:31:50 -0500

    Update 06_data_acquisition.sh

--- src/bioamla/_internal/examples/06_data_acquisition.sh
+++ src/bioamla/_internal/examples/06_data_acquisition.sh
@@ -20,9 +20,8 @@
 set -e  # Exit on error
 
 # Configuration
-PROJECT_NAME="frog_acoustic_study"
-PROJECT_DIR="./${PROJECT_NAME}"
-OUTPUT_DIR="${PROJECT_DIR}/acquired_data"
+
+OUTPUT_DIR="./acquired_data"
 SPECIES_COMMON="American Bullfrog"
 SPECIES_SCIENTIFIC="Lithobates catesbeianus"
 
